dd7e4ba4bbc5eb6f6f7fd90f1785c3bfc57ebbaa
Jeferson.Silva <jeferson.s.silva1@gmail.com>
2020-08-25 23:29:57 -0300

    Navegação

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Routes from './routes'
 
 import './styles.css'
 
@@ -8,7 +9,7 @@ import Header from './components/Header';
 const App = () => (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
   );
 
